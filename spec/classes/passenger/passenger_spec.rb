92bf2c2a3f91738af0a8262f3cbf0043cb031755
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2013-02-16 23:09:28 +0000

    Update tests

--- spec/classes/passenger/passenger_spec.rb
+++ spec/classes/passenger/passenger_spec.rb
@@ -6,7 +6,7 @@ describe 'passenger', :type => :class do
      it { should create_class('passenger::config')}
      it { should create_class('passenger::install')}
 
-     it { should contain_file('/etc/httpd/conf.d/passenger.conf').with_content(/# File Managed by Puppet/)}
+     it { should contain_file('/etc/httpd/conf.d/passenger.conf').with_source('puppet:///modules/passenger/passenger.conf')}
   end
   
 end
